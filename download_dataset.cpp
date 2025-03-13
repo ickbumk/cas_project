1dc4dc34eed8d3ae2c3f525765ada19d617c93d3
Ickbum Kim <escapefj@gmail.com>
2025-03-12 22:10:13 -0400

    adding dataset

---
+++ download_dataset.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+// defines a 3D vector, stores vertices and normals
+struct Vec3{
+    float x,y,z;
+};
+
+// defines 2d vector, stores texture coordinates
+struct Vec2{
+    float u,v;
+};
+
+// face is consisted of three points with texture and normal
+struct Face{
+    std::vector<int> ind_vertex;
+    std::vector<int> ind_texture;
+    std::vector<int> ind_normal;
+};
+
+class obj_loader{
+    //contains vectors to store list of these
+    public:
+        std::vector<Vec3> vertices;
+        std::vector<Vec2> texture_coords;
+        std::vector<Vec3> normals;
+        std::vector<Face> faces;
+
+        bool load(const std::string& filename){
+            std::ifstream file(filename);
+            if (!file.is_open()){
+                std::cerr << "Failed to open the .obj file: " << filename << std::endl;
+                return false;
+            }
+            std::string line;
+
+        }
+
+};
