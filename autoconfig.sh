5b9954d37f0299e7f98fc8051fedfeb9fc30a3ac
Ickbum Kim <escapefj@gmail.com>
2025-04-28 23:47:12 -0400

    added autoconfig

---
+++ autoconfig.sh
@@ -0,0 +1,17 @@
+BUILD_TYPE=${1:-Release}
+CXX_COMPILER=${2:-g++}
+
+if [ ! -d ../Catch2-install ]; then
+    git clone https://github.com/catchorg/Catch2 ../Catch2
+    rm -rf ../build/Catch2
+    cmake -S ../Catch2 -B ../build/Catch2 \
+        -DBUILD_TESTING=OFF \
+        -DCMAKE_INSTALL_PREFIX=../Catch2-install/
+    cmake --build ../build/Catch2 -j$(nproc) --target install
+fi
+
+cmake -S . -B ./build \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCatch2_ROOT=../Catch2-install \
+    -DCMAKE_CXX_COMPILER=$CXX_COMPILER
+cmake --build ./build
